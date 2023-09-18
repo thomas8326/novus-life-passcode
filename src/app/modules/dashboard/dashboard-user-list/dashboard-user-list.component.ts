56d347683d17cafb7a2039dc695ffce60087592a
thomas.shih <tt7919@gmail.com>
2023-09-18 19:19:19 +0800

    Feat: add user list

--- src/app/modules/dashboard/dashboard-user-list/dashboard-user-list.component.ts
+++ src/app/modules/dashboard/dashboard-user-list/dashboard-user-list.component.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard-user-list.component.scss'],
 })
 export class DashboardUserListComponent {
-  constructor() {
-    console.log('in');
-  }
+  fakeUsers = [
+    {
+      id: 1,
+      name: '施宏儒',
+      birthday: '1994/11/26',
+    },
+    {
+      id: 2,
+      name: '林欣汝',
+      birthday: '2000/09/23',
+    },
+    {
+      id: 3,
+      name: '林宥嘉',
+      birthday: '2000/05/13',
+    },
+    {
+      id: 4,
+      name: '張組豪',
+      birthday: '1994/11/25',
+    },
+
+    {
+      id: 5,
+      name: '姜濟民',
+      birthday: '1983/08/03',
+    },
+  ];
+
+  constructor() {}
 }
