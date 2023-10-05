31522cfad875416bc42d61b61255d8c9d9fb5646
thomas.shih <tt7919@gmail.com>
2023-10-05 19:22:33 +0800

    feat: add crystal pre selection page.

--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { LogoComponent } from 'src/app/components/logo/logo.component';
 import { ClientComponent } from 'src/app/modules/client/client.component';
 import { ClientRoutingModule } from './client-routing.module';
@@ -15,7 +17,13 @@ import { HomeComponent } from './home/home.component';
     NavigatorComponent,
     CrystalsComponent,
   ],
-  imports: [CommonModule, ClientRoutingModule, LogoComponent],
+  imports: [
+    CommonModule,
+    ClientRoutingModule,
+    LogoComponent,
+    MatIconModule,
+    FormsModule,
+  ],
   exports: [ClientComponent],
 })
 export class ClientModule {}
--- src/app/modules/client/crystals/crystals.component.ts
+++ src/app/modules/client/crystals/crystals.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-crystals',
   templateUrl: './crystals.component.html',
-  styleUrls: ['./crystals.component.scss']
+  styleUrls: ['./crystals.component.scss'],
 })
 export class CrystalsComponent {
-
+  gender: string = 'male';
 }
