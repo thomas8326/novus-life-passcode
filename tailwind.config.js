7f0e08cf14ae83d6340a104116228310f8e709de
tshih.tw <tshih.tw@ffn.com>
2024-01-09 23:34:41 +0800

    feat: update home page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e4f2f8",
+        secondary: "#ecc94b",
+      },
+    },
   },
   plugins: [],
 };
