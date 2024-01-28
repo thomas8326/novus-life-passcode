3e7819ab2bb52ba39a446a52bb077821d3cc0551
tshih.tw <tshih.tw@ffn.com>
2024-01-28 22:01:27 +0800

    feat: update font style

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import '@fontsource/open-sans'; // Defaults to weight 400
+import '@fontsource/open-sans/400-italic.css'; // Specify weight and style
+import '@fontsource/open-sans/400.css'; // Specify weight
+import '@fontsource/playpen-sans';
+
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic()
