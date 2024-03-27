38fcf39ec39fe71aaaa0d6a80391cead762b0cd9
tshih.tw <tshih.tw@ffn.com>
2024-03-28 00:01:08 +0800

    feat: redirect to home

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-logo',
   standalone: true,
-  imports: [],
-  template: `<div class="text-[36px] font-bold">Novus Crystal Life</div>`,
+  imports: [RouterLink],
+  template: `<a class="text-[36px] font-bold" routerLink="home"
+    >Novus Crystal Life</a
+  >`,
   styles: ``,
 })
 export class LogoComponent {}
