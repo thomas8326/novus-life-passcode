b6b36340214a9c12377aa53bb724b9af3c5d4c36
tshih.tw <tshih.tw@ffn.com>
2024-04-07 23:27:29 +0800

    feat: add logo

--- src/app/components/logo/logo.component.ts
+++ src/app/components/logo/logo.component.ts
@@ -5,9 +5,16 @@ import { RouterLink } from '@angular/router';
   selector: 'app-logo',
   standalone: true,
   imports: [RouterLink],
-  template: `<a class="text-[36px] font-bold" routerLink="home"
-    >Novus Crystal Life</a
-  >`,
+  template: `
+    <a class="text-[36px] font-bold flex gap-2" routerLink="home">
+      <img
+        src="assets/logo/novus-no-border.png"
+        alt="Novus Crystal Life"
+        class="w-[60px] h-[60px] border border-[#b5d7e2] rounded-full p-2 object-cover"
+      />
+      <div>Novus Crystal Life</div>
+    </a>
+  `,
   styles: ``,
 })
 export class LogoComponent {}
