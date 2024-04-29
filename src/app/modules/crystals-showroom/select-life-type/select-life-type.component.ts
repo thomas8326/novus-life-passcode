42b3d51d82ec4c352522080a6803ae64747d61ab
tshih.tw <tshih.tw@ffn.com>
2024-04-29 22:11:20 +0800

    feat: select life type mobile view.

--- src/app/modules/crystals-showroom/select-life-type/select-life-type.component.ts
+++ src/app/modules/crystals-showroom/select-life-type/select-life-type.component.ts
@@ -1,10 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 import { GenderSelectionComponent } from 'src/app/components/gender-selection/gender-selection.component';
 import { LifeType } from 'src/app/enums/life-type.enum';
+import { ResponsiveService } from 'src/app/services/responsive/responsive.service';
 
 @Component({
   selector: 'app-select-life-type',
@@ -15,10 +16,13 @@ import { LifeType } from 'src/app/enums/life-type.enum';
     FormsModule,
     CommonModule,
     GenderSelectionComponent,
+    AsyncPipe,
   ],
   templateUrl: './select-life-type.component.html',
   styles: ``,
 })
 export class SelectLifeTypeComponent {
   LifeType = LifeType;
+
+  constructor(public responsive: ResponsiveService) {}
 }
