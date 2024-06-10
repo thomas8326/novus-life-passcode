282dd880496e5a80bb83bdf20e73673d56b8c1bf
tshih.tw <tshih.tw@ffn.com>
2024-06-10 23:18:08 +0800

    feat: my account page.

--- src/app/components/update-account/update-account-dialog.component.ts
+++ src/app/components/update-account/update-account-dialog.component.ts
@@ -1,16 +1,11 @@
-import { Component, Inject, OnInit, signal } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { Account } from 'src/app/models/account';
-import { AccountService } from 'src/app/services/account/account.service';
+import { UpdateAccountComponent } from 'src/app/components/update-account/update-account.component';
 import { ResponsiveService } from 'src/app/services/responsive/responsive.service';
-import {
-  numericValidator,
-  taiwanPhoneValidator,
-} from 'src/app/validators/numberic.validators';
 
 @Component({
   selector: 'app-update-account-dialog',
@@ -20,67 +15,13 @@ import {
     MatInputModule,
     MatFormFieldModule,
     MatButtonModule,
+    UpdateAccountComponent,
   ],
   template: `
-    <form
-      [formGroup]="form"
-      class="flex flex-col w-full h-full px-2 py-4 overflow-hidden"
-    >
-      <h2
-        class="text-center !my-8 flex-none lg:text-desktopSubTitle text-mobileTitle font-bold"
-      >
-        輸入基本資料
-      </h2>
-      <div class="flex-1">
-        <div class="flex flex-col w-full h-full">
-          <div class="flex flex-col gap-1 px-2 flex-1">
-            <mat-form-field appearance="outline">
-              <mat-label>FB名稱/姓名</mat-label>
-              <input matInput formControlName="name" />
-              @if (this.form.controls.name.hasError('required')) {
-                <mat-error>請入入FB名稱或姓名</mat-error>
-              }
-            </mat-form-field>
-            <mat-form-field appearance="outline">
-              <mat-label>聯絡電話</mat-label>
-              <input matInput formControlName="phone" />
-              @if (this.form.controls.phone.hasError('required')) {
-                <mat-error>請輸入手機</mat-error>
-              } @else if (this.form.controls.phone.hasError('numeric')) {
-                <mat-error>請輸入數字</mat-error>
-              } @else if (this.form.controls.phone.hasError('invalidPhone')) {
-                <mat-error>請輸入台灣電話</mat-error>
-              }
-            </mat-form-field>
-            <mat-form-field appearance="outline" class="flex-none !w-32">
-              <mat-label>郵遞區號</mat-label>
-              <input matInput formControlName="zipCode" />
-              @if (this.form.controls.zipCode.hasError('required')) {
-                <mat-error>請輸入郵遞區號</mat-error>
-              } @else if (this.form.controls.zipCode.hasError('numeric')) {
-                <mat-error>請輸入數字</mat-error>
-              }
-            </mat-form-field>
-            <mat-form-field appearance="outline">
-              <mat-label>地址</mat-label>
-              <input matInput formControlName="address" />
-              @if (this.form.controls.address.hasError('required')) {
-                <mat-error>請輸入地址</mat-error>
-              }
-            </mat-form-field>
-          </div>
-        </div>
-      </div>
-      <div class="flex justify-end">
-        <button
-          mat-flat-button
-          class="!bg-highLight !hover:bg-highLightHover !text-white !w-24 !h-12 !text-mobileSubTitle !lg:text-desktopSubTitle"
-          (click)="onUpdate()"
-        >
-          更新
-        </button>
-      </div>
-    </form>
+    <app-update-account
+      [account]="{ uid: dialogData.uid, email: dialogData.email }"
+      (afterUpdated)="dialogRef.close()"
+    ></app-update-account>
   `,
   styles: `
     :host {
@@ -89,36 +30,16 @@ import {
     }
   `,
 })
-export class UpdateAccountDialogComponent implements OnInit {
-  private account = signal<Partial<Account>>({});
-
-  form = this.fb.group({
-    name: ['', Validators.required],
-    phone: [
-      '',
-      [Validators.required, numericValidator(), taiwanPhoneValidator()],
-    ],
-    email: ['', Validators.email],
-    zipCode: ['', [Validators.required, numericValidator()]],
-    address: ['', Validators.required],
-  });
-
+export class UpdateAccountDialogComponent {
   constructor(
     public dialogRef: MatDialogRef<UpdateAccountDialogComponent>,
-    private fb: FormBuilder,
-    private readonly accountService: AccountService,
     @Inject(MAT_DIALOG_DATA)
-    private readonly dialogData: {
-      uid?: string;
-      email?: string;
+    public readonly dialogData: {
+      uid: string;
+      email: string;
     },
     private readonly response: ResponsiveService,
   ) {
-    if (this.dialogData?.uid) {
-      const { uid, email } = this.dialogData;
-      this.account.update((prev) => ({ ...prev, uid, email }));
-    }
-
     this.response.getDeviceObservable().subscribe((device) => {
       if (device.desktop) {
         this.dialogRef.updateSize('410px', '630px');
@@ -126,30 +47,5 @@ export class UpdateAccountDialogComponent implements OnInit {
         this.dialogRef.updateSize('100%', 'auto');
       }
     });
-  }
-
-  onUpdate() {
-    this.form.markAllAsTouched();
-
-    if (this.form.invalid) {
-      return;
-    }
-
-    const updatedAccount = {
-      ...this.form.value,
-      uid: this.account().uid,
-    } as Account;
-
-    this.accountService.updateUserAccount(updatedAccount).then(() => {
-      const account = this.account();
-      account.uid && this.accountService.fetchMyAccount(account.uid);
-      this.dialogRef.close();
-    });
-  }
-
-  ngOnInit(): void {
-    if (this.account) {
-      this.form.patchValue(this.account());
-    }
   }
 }
--- src/app/modules/account/account.component.ts
+++ src/app/modules/account/account.component.ts
@@ -3,16 +3,38 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { MessageSnackbarComponent } from 'src/app/components/message-snackbar/message-snackbar.component';
+import { UpdateAccountComponent } from 'src/app/components/update-account/update-account.component';
 import { AccountService } from 'src/app/services/account/account.service';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, AsyncPipe],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    AsyncPipe,
+    UpdateAccountComponent,
+    MatSnackBarModule,
+  ],
   templateUrl: './account.component.html',
   styles: ``,
 })
 export class AccountComponent {
   private readonly account = inject(AccountService);
+  private readonly snackBar = inject(MatSnackBar);
   myAccount$ = this.account.myAccount$;
+
+  onUpdated() {
+    this.snackBar.openFromComponent(MessageSnackbarComponent, {
+      data: {
+        message: '已成功更新',
+        messageType: 'success',
+      },
+      horizontalPosition: 'end',
+      duration: 600,
+    });
+  }
 }
