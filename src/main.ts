62d406c7c8b8a95ddabdc36f328b9a9f51034598
tshih.tw <tshih.tw@ffn.com>
2024-08-01 00:29:39 +0800

    feat: Update font family

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-import '@fontsource/open-sans'; // Defaults to weight 400
-import '@fontsource/open-sans/400-italic.css'; // Specify weight and style
-import '@fontsource/open-sans/400.css'; // Specify weight
-import '@fontsource/playpen-sans';
-
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
