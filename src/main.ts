1818be47e5918407b7386e5a9fa2e09b678b3405
tshih.tw <tshih.tw@ffn.com>
2024-08-06 23:45:12 +0800

    chore: update font

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import '@fontsource-variable/noto-sans-tc';
-import '@fontsource/spectral';
 import { AppComponent } from 'src/app/app.component';
 import { appConfig } from 'src/app/app.config';
 
