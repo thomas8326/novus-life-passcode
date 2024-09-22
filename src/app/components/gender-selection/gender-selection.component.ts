426b3a56b203ab19ec387c8a365f2c34f48ec8bf
tshih.tw <tshih.tw@ffn.com>
2024-09-22 23:29:57 +0800

    feat: some layout update.

--- src/app/components/gender-selection/gender-selection.component.ts
+++ src/app/components/gender-selection/gender-selection.component.ts
@@ -12,8 +12,7 @@ import { Gender } from 'src/app/enums/gender.enum';
       class="flex relative border border-gray-200 rounded overflow-hidden w-[200px] h-[80px]"
     >
       <label
-        class="w-full h-full flex items-center justify-center cursor-pointer"
-        [ngClass]="{ 'bg-blue-300': gender() === 'male' }"
+        class="w-full h-full flex items-center justify-center cursor-pointer has-[:checked]:bg-blue-300 bg-stone-200"
         (click)="onSelectGender(Gender.Male)"
       >
         <mat-icon
@@ -21,16 +20,21 @@ import { Gender } from 'src/app/enums/gender.enum';
           class="text-[30px] !w-[30px] !h-[30px]"
           [ngClass]="{ 'text-white': gender() === 'male' }"
         ></mat-icon>
-        <input class="hidden" type="radio" name="gender" value="male" />
+        <input
+          class="hidden"
+          type="radio"
+          name="gender"
+          value="male"
+          [checked]="gender() === Gender.Male"
+        />
       </label>
       <div
-        class="absolute px-1.5 w-fit h-fit inset-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded bg-[#f6f6f6]"
+        class="absolute px-1.5 w-fit h-fit inset-1/2 transform -translate-x-1/2 -translate-y-1/2 border rounded bg-gray-600 text-white"
       >
         OR
       </div>
       <label
-        class="w-full h-full flex items-center justify-center cursor-pointer"
-        [ngClass]="{ 'bg-red-300': gender() === 'female' }"
+        class="w-full h-full flex items-center justify-center cursor-pointer has-[:checked]:bg-red-300 bg-stone-200"
         (click)="onSelectGender(Gender.Female)"
       >
         <mat-icon
@@ -38,7 +42,13 @@ import { Gender } from 'src/app/enums/gender.enum';
           class="text-[30px] !w-[30px] !h-[30px]"
           [ngClass]="{ 'text-white': gender() === 'female' }"
         ></mat-icon>
-        <input class="hidden" type="radio" name="gender" value="female" />
+        <input
+          class="hidden"
+          type="radio"
+          name="gender"
+          value="female"
+          [checked]="gender() === Gender.Female"
+        />
       </label>
     </div>
   </div>`,
